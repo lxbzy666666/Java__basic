3ba92706be65dde4862c0f9cf5b8eef8e7d62f7a
lxb <3564489495@qq.com>
2021-02-01 15:33:52 +0800

    测试

---
+++ base/src/main/java/threaddemo/ThreadDemoTest.java
@@ -0,0 +1,21 @@
+package threaddemo;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @Author Gary luo
+ * @Date 2021/2/1 15:19
+ * @Version 1.0
+ */
+public class ThreadDemoTest {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 采用非安全的方式进行临界资源的修改
+     */
+    public  static  void  notSageModify(){
+
+    }
+}
